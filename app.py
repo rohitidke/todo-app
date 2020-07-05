2db4a416bd14dbe988e13f3e368f1470de79b66b
rohitidke <rtrohittidke3@gmail.com>
2020-07-05 23:31:52 +0530

    added fetaure of todo list

--- app.py
+++ app.py
@@ -18,14 +18,29 @@ class Todo(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	description = db.Column(db.String(), nullable=False)
 	completed = db.Column(db.Boolean, nullable=False, default=False)
+	list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
 	def __repr__(self):
 		return f'<todo: id-{self.id}, description-{self.description}'
 
 
+class TodoList(db.Model):
+	__tablename__='todolists'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(), nullable=False)
+	todos = db.relationship("Todo", backref='list', lazy=True)
+
+
 @app.route('/')
 def index():
-	return render_template('index.html', data=Todo.query.all())
+	return redirect(url_for('get_list_todos', list_id=1))
+
+
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+	return render_template('index.html', todos=Todo.query.filter_by(list_id=list_id).order_by('id').all(),
+		lists=TodoList.query.order_by('id').all(), active_list=TodoList.query.get(list_id))
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -48,6 +63,47 @@ def create_todo():
 
 	if not error:
 		return jsonify(body)
+	else:
+		abort(500)
+
+
+@app.route('/todos/<todo_id>/setCompleted', methods=['POST'])
+def set_completed(todo_id):
+	error = False
+	try:
+		completed = request.get_json()['completed']
+		todo = Todo.query.get(todo_id)
+		todo.completed = completed
+		db.session.commit()
+	except:
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+
+	if not error:
+		return redirect(url_for('index'))
+	else:
+		abort(500)
+
+
+@app.route('/todos/<todo_id>/delete', methods=['DELETE'])
+def delete_todo(todo_id):
+	error = False
+	try:
+		todo = Todo.query.get(todo_id)
+		db.session.delete(todo)
+		db.session.commit()
+	except:
+		error = True
+		db.session.rollback()
+		print(sys.exc_info())
+	finally:
+		db.session.close()
+
+	if not error:
+		return jsonify({ 'success': True })
 	else:
 		abort(500)
 
